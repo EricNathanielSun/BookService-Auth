92c991e67c61289d1e8c6689fb7c5f1f2d4503fe
Eric <eric.sun@pointwest.com.ph>
2018-09-03 16:48:39 +0800

    Auth Aspect now has logging for service method start and end

--- src/main/java/pw/io/booker/service/AuthenticationAspect.java
+++ src/main/java/pw/io/booker/service/AuthenticationAspect.java
@@ -1,8 +1,12 @@
 package pw.io.booker.service;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 import pw.io.booker.exception.InvalidAuthException;
 import pw.io.booker.model.Authentication;
@@ -11,10 +15,12 @@ import pw.io.booker.model.Authentication;
 public class AuthenticationAspect {
 	
 	private AuthenticationService authenticationService;
+	private Logger logger;
 	
 	public AuthenticationAspect(AuthenticationService authenticationService) {
 		super();
 		this.authenticationService = authenticationService;
+		logger = Logger.getLogger(AuthenticationAspect.class);
 	}
 
 
@@ -25,14 +31,27 @@ public class AuthenticationAspect {
 		Authentication authentication;
 		if(token == null || "".equals(token)) // is null or blank
 		{
+			logger.debug("Access Request denied: Token is blank");
 			throw new InvalidAuthException("Token is blank");
 		}
 		authentication = authenticationService.findByToken(token);
 		if(authentication == null)
 		{
+			logger.debug("Access Request denied: Token is invalid");
 			throw new InvalidAuthException("Invalid Token");
 		}
 		return proceedingJoinPoint.proceed();
 	}
 	
+	@Before("execution(* pw.io.booker.service..*(..))")
+	public void logServiceMethodStart(JoinPoint joinPoint)
+	{
+		logger.info("Method started: " + joinPoint.getSignature().getName());
+	}
+	
+	@After("execution(* pw.io.booker.service..*(..))")
+	public void logServiceMethodEnd(JoinPoint joinPoint)
+	{
+		logger.info("Method ended: " + joinPoint.getSignature().getName());
+	}
 }
